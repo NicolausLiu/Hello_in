7d2446c9d2c0936cfad38127ae7be3214743a0e0
NicolausLiu <43713126+NicolausLiu@users.noreply.github.com>
2020-11-18 16:36:48 +0800

    update

--- main.py
+++ main.py
@@ -12,5 +12,6 @@ def print_hi(name):
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyCharm')
+    print("How to become a master of life?\n Be peaceful to loss and gain in life may be a way of cheating for a numb man. ")
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
